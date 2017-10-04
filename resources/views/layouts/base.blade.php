31f84c1ea7e3d0cd2bddc3e24b358271e9e0f952
= <prianikovkirill@gmail.com>
2017-10-04 10:32:34 +0300

    drivers page and menu page

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Untitled Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/tether/tether.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/main.css')}}">
+</head>
+
+<body>
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><br>
+                <img src="images/logo.gif" width="60" height="32" alt="" class="img-responsive" style="width:80px;"/>
+                <br>
+            </div>
+            @yield('password_link')
+        </div>
+    </div>
+</header>
+    @yield('content')
+<script src="{{asset('js/jquery/jquery-3.2.1.min.js')}}"></script>
+<script src="{{asset('js/tether/tether.min.js')}}"></script>
+<script src="{{asset('js/bootstrap/bootstrap.min.js')}}"></script>
+    @yield('scripts')
+</body>
+</html>
