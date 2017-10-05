8b78ea3a0c2e06b60d363de26dd9b119aec48fcc
= <prianikovkirill@gmail.com>
2017-10-05 10:53:44 +0300

    bookings and missed front done

---
+++ resources/views/menu/options/booking_detail.blade.php
@@ -0,0 +1,157 @@
+@extends('menu.menu_body')
+
+@section('menu_options')
+    <div class="container">
+        <br>
+        <div class="row" id="white">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <p class=""><a href="{{//route('menu')}}">Drivers Names</a> &gt; <a href="{{//route('personal_menu',$driver['_id'])}}">{{//$driver['full_name']}}</a> &gt; <a href="{{//route('menu_booking', $driver['_id'])}}">Bookings</a> &gt; Detail</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+
+        <!----------------------------------------->
+        <div class="row" id="gray">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="btn-1" style="background-color:#FFF;">
+                <h3 id="status" style="text-align:center">KHI8986580</h3>
+            </div>
+        </div>
+        <!----------------------------------------->
+
+        <!----------------------------------------->
+        <div class="row" id="gray">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="btn-1">
+                <h3 id="status" style="font-weight:100;font-size:16px;"><span style="font-weight:bold">Pickup: </span>Khayaban-e-Ittihad</h3>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="btn-1">
+                <h3 id="status" style="font-weight:100;font-size:16px;"><span style="font-weight:bold">Drop Off: </span>Saddar Town</h3>
+            </div>
+        </div>
+        <!----------------------------------------->
+        <!----------------------------------------->
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <h3 id="status" class="pull-left">Booking Time</h3>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <h3 id="status" class="pull-right" style="font-weight:100;font-size:16px">22 aug, 10:38 AM</h3>
+            </div>
+        </div>
+        <div class="row" id="white">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <h3 id="status" class="pull-left">Service Type</h3>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <h3 id="status" class="pull-right" style="font-weight:100;font-size:16px">Ride</h3>
+            </div>
+        </div>
+
+        <!----------------------------------------->
+        <!----------------------------------------->
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <h3 id="status" class="pull-left">12 km</h3>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <h3 id="status" class="pull-right" style="font-weight:100;font-size:16px">48</h3>
+            </div>
+        </div>
+
+        <!----------------------------------------->
+        <!----------------------------------------->
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <h3 id="status" class="pull-left">28 min</h3>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <h3 id="status" class="pull-right" style="font-weight:100;font-size:16px">42</h3>
+            </div>
+        </div>
+
+        <!----------------------------------------->
+        <!----------------------------------------->
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <h3 id="status" class="pull-left">Base Fare</h3>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <h3 id="status" class="pull-right" style="font-weight:100;font-size:16px">35</h3>
+            </div>
+        </div>
+
+        <!----------------------------------------->
+        <!----------------------------------------->
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <h3 id="status" class="pull-left">Fare</h3>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <h3 id="status" class="pull-right" style="font-weight:100;font-size:16px">125</h3>
+            </div>
+        </div>
+
+        <!----------------------------------------->
+        <!----------------------------------------->
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <h3 id="status" class="pull-left">Promo</h3>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <h3 id="status" class="pull-right" style="font-weight:100;font-size:16px">0</h3>
+            </div>
+        </div>
+
+        <!----------------------------------------->
+        <!----------------------------------------->
+        <div class="row" id="white">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <h3 id="status" class="pull-left">Wallet</h3>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <h3 id="status" class="pull-right" style="font-weight:100;font-size:16px">-100</h3>
+            </div>
+        </div>
+
+        <!----------------------------------------->
+        <!----------------------------------------->
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <h3 id="status" class="pull-left" style="font-size:24px;">Total</h3>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <h3 id="status" class="pull-right" style="font-weight:100;font-size:16px;font-size:24px">Rs 125</h3>
+            </div>
+        </div>
+
+        <!----------------------------------------->
+        <!----------------------------------------->
+        <div class="row" id="white">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <h3 id="status" class="pull-left green">Amount Top Up</h3>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <h3 id="status" class="pull-right" style="font-weight:100;font-size:16px">Rs. 125</h3>
+            </div>
+        </div>
+
+        <!----------------------------------------->
+
+    </div>
+@endsection
+
+@section('options_scripts')
+    {{--<script>
+        $('#bookings').click(function (e) {
+            e.preventDefault();
+            $(this).tab('show');
+            console.log('Bookings selected');
+        })
+        $('#cancelled').click(function (e) {
+            e.preventDefault();
+            $(this).tab('show');
+            console.log('Cancelled selected');
+        })
+    </script>--}}
+@endsection
---
+++ resources/views/menu/options/profile_page.blade.php
@@ -0,0 +1,184 @@
+@extends('menu.menu_body')
+
+@section('menu_options')
+    <div class="container">
+        <div class="row white">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <p class="breadcrumb"><a href="{{route('menu')}}">Drivers Names</a> &gt; <a href="{{route('personal_menu', $driver['_id'])}}">{{$driver['full_name']}}</a> &gt; Profile</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <!----------------------------------------->
+        <div class="row white">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 btn-1" style="background-color:#FFF;">
+                <h1 class="status" style="text-align:center">Settings</h1>
+            </div>
+        </div>
+        <!----------------------------------------->
+        <!----------------------------------------->
+        <a href="settings-change.php">
+            <div class="row white">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    <h3 class="driver-name">GPS</h3>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                    <h3 class="driver-name">{{$driver['last_lat']}}&nbsp;{{$driver['last_lng']}}</h3>
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"><i class="fa fa-arrow-right pull-right next"></i>
+                </div>
+            </div>
+        </a>
+        <!----------------------------------------->
+        <!----------------------------------------->
+        <a href="settings-change.php">
+            <div class="row white">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    <h3 class="driver-name">Address</h3>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                    <h3 class="driver-name">{{$driver['address']}}</h3>
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"><i class="fa fa-arrow-right pull-right next"></i>
+                </div>
+            </div>
+        </a>
+        <!----------------------------------------->
+        <!----------------------------------------->
+        <a href="settings-change.php">
+            <div class="row white">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    <h3 class="driver-name">City</h3>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                    <h3 class="driver-name">{{$driver['address']}}</h3>
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"><i class="fa fa-arrow-right pull-right next"></i>
+                </div>
+            </div>
+        </a>
+        <!----------------------------------------->
+        <!----------------------------------------->
+        <a href="settings-change.php">
+            <div class="row white">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    <h3 class="driver-name">License #</h3>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                    <h3 class="driver-name">{{$driver['driver_license_number']}}</h3>
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"><i class="fa fa-arrow-right pull-right next"></i>
+                </div>
+            </div>
+        </a>
+        <!----------------------------------------->
+        <!----------------------------------------->
+        <a href="settings-change.php">
+            <div class="row white">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    <h3 class="driver-name">Mobile (Company Provided)</h3>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                    <h3 class="driver-name">{{$driver['phone']}}</h3>
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"><i class="fa fa-arrow-right pull-right next"></i>
+                </div>
+            </div>
+        </a>
+        <!-----------------------------------------> <!----------------------------------------->
+        <a href="settings-change.php">
+            <div class="row white">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    <h3 class="driver-name">Mobile (Personal 1)</h3>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                    <h3 class="driver-name">{{$driver['mobile_1']}}</h3>
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"><i class="fa fa-arrow-right pull-right next"></i>
+                </div>
+            </div>
+        </a>
+        <!-----------------------------------------> <!----------------------------------------->
+        <a href="settings-change.php">
+            <div class="row white">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    <h3 class="driver-name">Mobile (Personal 2)</h3>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                    <h3 class="driver-name">{{$driver['mobile_2']}}</h3>
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"><i class="fa fa-arrow-right pull-right next"></i>
+                </div>
+            </div>
+        </a>
+        <!----------------------------------------->
+        <!----------------------------------------->
+        <a href="settings-change.php">
+            <div class="row white">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    <h3 class="driver-name">Email</h3>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                    <h3 class="driver-name">{{$driver['email']}}</h3>
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"><i class="fa fa-arrow-right pull-right next"></i>
+                </div>
+            </div>
+        </a>
+        <!----------------------------------------->
+        <!----------------------------------------->
+        <a href="settings-change.php">
+            <div class="row white">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    <h3 class="driver-name">Activation</h3>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    <h3 class="driver-name">{{$driver['updated_at']}}</h3>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    <input type="checkbox" data-id="{{$driver['_id']}}" class="pull-right">
+                    <h3 class="driver-name pull-right red"> Deactive</h3>
+                </div>
+            </div>
+        </a>
+        <!----------------------------------------->
+
+    </div>
+
+@endsection
+
+@section('options_scripts')
+    <script>
+        $(document).ready(function () {
+            $('input[type="checkbox"]').on('click', function (e) {
+                var id = $(this).data('id');
+                var data ={};
+                var isChecked = $(this).is(":checked");
+                console.log(id);
+                if (isChecked)
+                {
+                    data = {id: id, activated: true};
+                } else {
+                    data = {id: id, activated: false};
+                }
+
+                $.ajax({
+                    url: "#",
+                    method: 'post',
+                    data: data
+                }).done(function() {
+                    if (isChecked) {
+                        $( this ).siblings('driver-name').addClass('red').text('Deactivate');
+                        $( this ).siblings('driver-name').removeClass('green').text('Activate');
+                    } else {
+                        $( this ).siblings('driver-name').addClass('green').text('Activate');
+                        $( this ).siblings('driver-name').removeClass('red').text('Deactivate');
+                    }
+
+                });
+
+            })
+        })
+    </script>
+@endsection
