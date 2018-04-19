fb72f799c3484388f3b85685c9af0c57b9e0ad79
avadakkeddavra <avadakkeddavra@gmail.com>
2018-04-19 12:11:53 +0300

    fixed last one errors

--- resources/views/menu/options/uploaded_statements.blade.php
+++ resources/views/menu/options/uploaded_statements.blade.php
@@ -23,6 +23,14 @@
                     {{ session('status') }}
                 </div>
             @endif
+                @if(session('error'))
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        {{ session('error') }}
+                    </div>
+                @endif
             <h4>Statments</h4>
             <table class="table">
 
